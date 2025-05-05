481f1ce80c61d657c5d78d0597c2ba1ea7091871
molinajr11 <molinajunior1111@gmail.com>
2025-05-05 17:03:46 -0500

    19 desesctructuracion argumentos

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,16 @@
+export {}
+
+interface Product {
+    description : string;
+    price : number;
+}
+
+const phone : Product = {
+    description : 'Black berry',
+    price:50.0
+}
+
+const tablet: Product = {
+    description: 'Sam',
+    price: 2
+}
