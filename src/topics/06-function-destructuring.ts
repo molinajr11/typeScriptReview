8cca33ffa8cbb0d4c8434709b9f826baede4ac4b
molinajr11 <molinajunior1111@gmail.com>
2025-05-07 16:44:37 -0500

    Desestructuracion argumentos

--- src/topics/06-function-destructuring.ts
+++ src/topics/06-function-destructuring.ts
@@ -14,3 +14,28 @@ const tablet: Product = {
     description: 'Sam',
     price: 2
 }
+
+interface TaxCalculationOptions {
+    tax: number;
+    products : Product []
+}
+function taxCalculator(options:TaxCalculationOptions){
+    let total = 0;
+
+    options.products.forEach(p =>{
+        total += p.price
+    })
+    return [total,total * options.tax]
+}
+
+const shoppingCar = [phone,tablet] ;
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculator ({
+    products: shoppingCar,
+    tax,
+
+});
+
+console.log('Total',total)
+console.log ('Total',taxTotal)
